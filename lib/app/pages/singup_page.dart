737ef1e4c6f582f656133afb288fc1dad70dd81a
TiCefops <SistemasCefops@2022>
2022-02-05 10:53:53 -0300

    dsf

--- lib/app/pages/singup_page.dart
+++ lib/app/pages/singup_page.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:cefops2/app/controller/singup_controller.dart';
 import 'package:cefops2/app/routes/app_routes.dart';
@@ -25,112 +24,114 @@ class SingupPage extends GetView<SingupController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.blue,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: Get.height,
-          child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-            double widethForPC = constraints.maxWidth;
-            if (constraints.maxWidth >= 600) {
-              widethForPC = constraints.maxWidth * 0.5;
-            }
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: constraints.maxHeight * 0.08,
-                ),
-                Center(
-                  child: SizedBox(
-                    width: constraints.maxWidth * 0.3,
-                    height:constraints.maxHeight * 0.15,
-                    child: Image.asset(
-                      "assets/image/brasao.png",
+      body: SizedBox(
+        height: Get.height,
+        child: LayoutBuilder(
+
+            builder: (BuildContext context, BoxConstraints constraints) {
+              double widethForPC = constraints.maxWidth;
+              if (constraints.maxWidth >= 600) {
+                widethForPC = constraints.maxWidth * 0.5;
+              }
+              return Column(
+
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    height: Get.height * 0.08,
+                  ),
+                  Center(
+                    child: SizedBox(
+                      width: Get.width * 0.3,
+                      height: Get.height * 0.15,
+                      child: Image.asset(
+                        "assets/image/brasao.png",
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: widethForPC,
-                  height: constraints.maxHeight,
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        FormLoginWidget("Nome Completo", Icons.person,
-                            nomeController, false),
-                        FormLoginWidget(
-                            "CPF", Icons.person, cpfController, false),
-                        FormLoginWidget(
-                            "E-mail", Icons.person, emailController, false),
-                        FormLoginWidget(
-                            "Senha", Icons.lock, passowrdController, true),
-                        FormLoginWidget("Repita sua Senha", Icons.lock,
-                            passowrd2Controller, true),
-                      ],
+                  Expanded(
+
+                    child: SizedBox(
+                      width: widethForPC,
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            FormLoginWidget("Nome Completo", Icons.person,
+                                nomeController, false),
+                            FormLoginWidget(
+                                "CPF", Icons.person, cpfController, false),
+                            FormLoginWidget(
+                                "E-mail", Icons.person, emailController, false),
+                            FormLoginWidget(
+                                "Senha", Icons.lock, passowrdController, true),
+
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Obx(
-                  () {
-                    return controller.loadingPage.value
-                        ? const CircularProgressIndicator(
-                            backgroundColor: Colors.white,
-                            color: AppColors.orange,
-                          )
-                        : ElevatedButton(
-                            onPressed: () async {
-                              String email = emailController.text;
-                              String passowrd = passowrdController.text;
-                              controller.name.value=nomeController.text;
-                              controller.cpf.value=cpfController.text;
-                              controller.email.value=emailController.text;
+                  Obx(
+                        () {
+                      return controller.loadingPage.value
+                          ? const CircularProgressIndicator(
+                        backgroundColor: Colors.white,
+                        color: AppColors.orange,
+                      )
+                          : ElevatedButton(
+                        onPressed: () async {
+                          String email = emailController.text;
+                          String passowrd = passowrdController.text;
+                          controller.name.value=nomeController.text;
+                          controller.cpf.value=cpfController.text;
+                          controller.email.value=emailController.text;
 
 
-                              if (_formKey.currentState!.validate()) {
-                                controller.loadingPage.value = true;
 
-                                await AuthenticationHelper()
-                                    .signUp(email: email, password: passowrd)
-                                    .then((result) async {
+                          if (_formKey.currentState!.validate()) {
+                            controller.loadingPage.value = true;
 
-                                  if (result == null) {
-                                    await AuthenticationHelper().
-                                    signIn(email: email, password: passowrd).
-                                    then((value) async => await controller.singup(
-                                    ));
+                            await AuthenticationHelper()
+                                .signUp(email: email, password: passowrd)
+                                .then((result) async {
+                              if (result == null) {
+                                await AuthenticationHelper().
+                                signIn(email: email, password: passowrd).
+                                then((value) async => await controller.singup(
+                                ));
 
-                                    controller.loadingPage.value = false;
-
-                                    Get.offAndToNamed(Routes.HOME);
-                                  } else {
-                                    controller.loadingPage.value = false;
-
-                                    Get.snackbar("Falha no Cadastro", result);
-                                  }
-                                });
                                 controller.loadingPage.value = false;
+
+                                Get.offAndToNamed(Routes.HOME);
+                              } else {
+                                controller.loadingPage.value = false;
+
+                                Get.snackbar("Falha no Cadastro", result);
                               }
-                            },
-                            child: const Text(
-                              "Cadastrar",
-                              style: TextStyle(color: AppColors.blue),
-                            ),
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.white),
-                                padding: MaterialStateProperty.all(
-                                    EdgeInsets.all(Get.height * 0.02)),
-                                textStyle: MaterialStateProperty.all(TextStyle(
-                                  fontSize: Get.height * 0.02,
-                                ))),
-                          );
-                  },
-                ),
-              ],
-            );
-          }),
-        ),
+                            });
+                            controller.loadingPage.value = false;
+                          }
+                        },
+                        child: const Text(
+                          "Cadastrar",
+                          style: TextStyle(color: AppColors.blue),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                            padding: MaterialStateProperty.all(
+                                EdgeInsets.all(Get.height * 0.02)),
+                            textStyle: MaterialStateProperty.all(TextStyle(
+                              fontSize: Get.height * 0.02,
+                            ))),
+                      );
+                    },
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
